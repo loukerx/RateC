dff019bd9ed74fa52ab35358a54aa9037600833b
Yin Hua <loukerx@gmail.com>
2015-10-28 15:33:02 +1100

    tableview cell auto sizing

--- RateC/CurrencyListTableViewController.swift
+++ RateC/CurrencyListTableViewController.swift
@@ -10,42 +10,63 @@ import UIKit
 
 class CurrencyListTableViewController: UITableViewController {
 
+    
+    var hotels:[String: String] = ["The Grand Del Mar": "5300 Grand Del Mar Court, San Diego, CA 92130",
+        "French Quarter Inn": "166 Church St, Charleston, SC 29401",
+        "Bardessono": "6526 Yount Street, Yountville, CA 94599",
+        "Hotel Yountville": "6462 Washington Street, Yountville, CA 94599",
+        "Islington Hotel": "321 Davey Street, Hobart, Tasmania 7000, Australia",
+        "The Henry Jones Art Hotel": "25 Hunter Street, Hobart, Tasmania 7000, Australia",
+        "Clarion Hotel City Park Grand": "22 Tamar Street, Launceston, Tasmania 7250, Australia",
+        "Quality Hotel Colonial Launceston": "31 Elizabeth St, Launceston, Tasmania 7250, Australia",
+        "Premier Inn Swansea Waterfront": "Waterfront Development, Langdon Rd, Swansea SA1 8PL, Wales",
+        "Hatcher's Manor": "73 Prossers Road, Richmond, Clarence, Tasmania 7025, Australia"]
+    
+    var hotelNames:[String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        hotelNames = [String](hotels.keys)
+        
+        print(hotelNames)
+        
+        
+        tableView.estimatedRowHeight = 68.0
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        
+        tableView.reloadData()
+        
     }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return hotels.count
     }
 
-    /*
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! CustomTableViewCell
+        
+        let hotelName = hotelNames[indexPath.row]
+        cell.nameLabel.text = hotelName
+        cell.addressLabel.text = hotels[hotelName]
+        
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
